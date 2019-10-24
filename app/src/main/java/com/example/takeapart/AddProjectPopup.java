a7a1e025f3d18fe74625eb2ea080e118475d1ebb
yuval0hering <yuval0hering@gmail.com>
2019-10-24 20:02:28 +0300

    add popup to create project

--- app/src/main/java/com/example/takeapart/AddProjectPopup.java
+++ app/src/main/java/com/example/takeapart/AddProjectPopup.java
@@ -2,10 +2,18 @@ package com.example.takeapart;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
-class AddProjectPopup extends Activity {
+public class AddProjectPopup extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.create_project_popup);
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        getWindow().setLayout((int)(width*0.8),(int)(height*0.6));
     }
 }
